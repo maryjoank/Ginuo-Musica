837a35542de7e380ebe5bdc86eee2e9c41fa4f30
maryjoank <74302156+maryjoank@users.noreply.github.com>
2023-09-03 12:55:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// My submission for learncssgarden.com challenge
+// === === ===
+// UI Inspired from: https://dribbble.com/afterglow-studio
+// svg icon:........ https://thenounproject.com
+// svg illustration: https://undraw.co/illustrations
